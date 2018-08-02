de6553abb71fb922aac6edac0c16a783a3245a2f
AlejoZepol <alejozepol@gmail.com>
2018-08-01 23:11:49 -0500

    CasadoSinConyugue

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ﻿# ConsultasKCT
+
+SQL SERVER
+
+CasadoSinConyugue : Con este script puede consultar en sql server los empleados activos que en el maestro de empleados (KBiEmple) 
+se encuentra como casado o union libre y en el maestro de familiares (KBiFamil) no tiene registrado su conyugue.
--- SQL/CasadoSinConyugue.sql
+++ SQL/CasadoSinConyugue.sql
@@ -1,3 +1,11 @@
+/*Con este script puede consultar en sql server los empleados que en el maestro de empleados (KBiEmple) 
+se encuentra como casado o union libre y en el maestro de familiares (KBiFamil) no tiene registrado su conyugue. */
+
+
+declare @ncod_empr smallint;
+/*asignar el codigo de la empresa que quiere consultar*/
+select @ncod_empr =409 --codigo empresa
+
 SELECT DISTINCT C.cod_ccos as 'Codigo Centro de Costo',
 				T.nom_ccos as 'Nombre Centro de Costo',
 				'Casado sin Conyugue registrados' as Tipo,
@@ -11,7 +19,7 @@ INNER JOIN BI_EMPLE B
   ON (C.COD_EMPR = B.COD_EMPR) AND (C.COD_EMPL = B.COD_EMPL)
   INNER JOIN gn_ccost T ON (C.cod_ccos=T.cod_ccos)
 INNER JOIN bi_cargo E ON (C.COD_CARG=E.COD_CARG)
-WHERE (C.COD_EMPR = 1)
+WHERE (C.COD_EMPR = @ncod_empr)
   AND (C.IND_ACTI <> 'I')
   AND (B.COD_EMPL <> 0)
   AND (B.EST_CIVI IN ('C', 'U'))
